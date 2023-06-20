15dce0300daa721e12472e159fc0eeb378e68000
william <124389603+william-simard@users.noreply.github.com>
2023-06-20 15:24:33 +0000

    almost done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 william simard All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: william simard
+// Created on: jun 2023
 // This file contains the JS functions for index.html
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then(response => response.json())
+  .then(data => {
+    const imageUrl = data.message;
+    // Use the imageUrl to display the image on your website
+    const imageElement = document.createElement("img");
+    imageElement.src = imageUrl;
+    document.body.appendChild(imageElement);
+  })
+  .catch(error => {
+    console.log("Error fetching dog image:", error);
+  });
